d81e0c83ddc065813a437f6285f2f308142aeef4
cfbonner <cfbonner@gmail.com>
2022-12-13 10:34:01 -0400

    Add joke routes and styles

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,24 @@
+export default function NewJokeRoute() {
+  return (
+    <div>
+      <h1>New joke</h1>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
